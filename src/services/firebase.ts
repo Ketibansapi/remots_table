8c29e280bfcf06f54d0dc98ca71e3f65b06864b9
Ketibansapi <deary2706@gmail.com>
2020-08-03 11:34:25 +0700

    import firebase config to project

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,11 +2,14 @@ import * as firebase from 'firebase';
 
 // TODO: fill in your firebase config
 const firebaseConfig = {
-    apiKey: '***********************',
-    authDomain: '***********************',
-    databaseURL: '***********************',
-    projectId: '***********************',
-    appId: '***********************'
+    apiKey: "AIzaSyBA124HKHncysA2XH6PXOZS23UsFmchKQQ",
+    authDomain: "remo-76c21.firebaseapp.com",
+    databaseURL: "https://remo-76c21.firebaseio.com",
+    projectId: "remo-76c21",
+    storageBucket: "remo-76c21.appspot.com",
+    messagingSenderId: "15993064127",
+    appId: "1:15993064127:web:a60c2d392790666695540a",
+    measurementId: "G-796BMWXP11"
 };
 
 firebase.initializeApp(firebaseConfig);
